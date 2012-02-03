0ff9564b5379c92d3d533b94ce815c75bd51d170
Troy Potts <tpotts@pfoglobal.com>
2012-02-03 17:21:41 -0600

    Add MarketingPage and OrderFrameChoice.

---
+++ Shared/MarketingPage.h
@@ -0,0 +1,13 @@
+//
+//  MarketingPage.h
+//  Smart-i
+//
+//  Created by Troy Potts on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BackgroundViewController.h"
+
+@interface MarketingPage : BackgroundViewController
+
+@end
---
+++ Shared/OrderFrameChoice.h
@@ -0,0 +1,13 @@
+//
+//  OrderFrameChoice.h
+//  Smart-i
+//
+//  Created by Troy Potts on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BackgroundViewController.h"
+
+@interface OrderFrameChoice : BackgroundViewController
+
+@end
